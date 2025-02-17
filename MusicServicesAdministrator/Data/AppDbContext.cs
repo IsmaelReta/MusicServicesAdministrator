0d3481331ccedb78936b8375d7df8f1b779454d3
IsmaelReta <ismaelreta.programacion@gmail.com>
2025-02-17 15:48:57 -0300

    DB connection and models added, pending relationships

---
+++ MusicServicesAdministrator/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicServicesAdministrator.Models.Entities;
+
+namespace MusicServicesAdministrator.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
---
+++ MusicServicesAdministrator/Models/Entities/Playlist.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicServicesAdministrator.Models.Entities
+{
+    public class Playlist
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
