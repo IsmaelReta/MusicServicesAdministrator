de73180ea9c83fa87d830cbdf796c028158b41c1
IsmaelReta <ismaelreta.programacion@gmail.com>
2025-02-21 15:44:09 -0300

    Now it realy works

--- MusicServicesManager/Controllers/PlatformsController.cs
+++ MusicServicesManager/Controllers/PlatformsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -68,161 +69,109 @@ namespace MusicServicesManager.Controllers
             
             
         }
+        
+        public async Task<IActionResult> Details(Guid id)
+        {
+            Platform platformDetails = new();
+
+            using (var _httpClient = new HttpClient())
+            {
+                _httpClient.BaseAddress = new Uri(baseURL + "api/platforms/");
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(
+                    new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage getData = await _httpClient.GetAsync($"{id}");
+
+                if (getData.IsSuccessStatusCode)
+                {
+                    string result = getData.Content.ReadAsStringAsync().Result;
+                    platformDetails = JsonConvert.DeserializeObject<Platform>(result)!;
+                }
+                else
+                {
+                    return View("ErrorPage");
+                }
+            }
+            return View(platformDetails);
+
+        }
+
+        public async Task<IActionResult> UpdatePlatform(Guid Id, Platform platformDTO)
+        {
+            using var _httpClient = new HttpClient();
+
+            _httpClient.BaseAddress = new Uri(baseURL + "api/platforms/");
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage getData = await _httpClient.PutAsJsonAsync($"{Id}", platformDTO);
+
+            if (getData.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("ErrorPage");
+            }
+
+
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            Platform platformDetails = new();
+
+            using (var _httpClient = new HttpClient())
+            {
+                _httpClient.BaseAddress = new Uri(baseURL + "api/platforms/");
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(
+                    new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage getData = await _httpClient.GetAsync($"{id}");
+
+                if (getData.IsSuccessStatusCode)
+                {
+                    string result = getData.Content.ReadAsStringAsync().Result;
+                    platformDetails = JsonConvert.DeserializeObject<Platform>(result)!;
+                }
+                else
+                {
+                    return View("ErrorPage");
+                }
+            }
+            return View(platformDetails);
+
+        }
+
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            using var _httpClient = new HttpClient();
+            
+                _httpClient.BaseAddress = new Uri(baseURL + "api/platforms/");
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(
+                    new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage getData = await _httpClient.DeleteAsync($"{id}");
+
+                if (getData.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View("ErrorPage");
+                }
+            
+        }
+
         public IActionResult ErrorPage()
         {
             return View();
         }
     }
 }
-
-
-/*
-namespace MusicServicesManager.Controllers
-{
-    public class PlatformsController : Controller
-    {
-        private readonly MusicServicesManagerContext _context;
-
-        public PlatformsController(MusicServicesManagerContext context)
-        {
-            _context = context;
-        }
-
-        // GET: Platforms
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Platform.ToListAsync());
-        }
-
-        // GET: Platforms/Details/5
-        public async Task<IActionResult> Details(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var platform = await _context.Platform
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (platform == null)
-            {
-                return NotFound();
-            }
-
-            return View(platform);
-        }
-
-        // GET: Platforms/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Platforms/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Platform platform)
-        {
-            if (ModelState.IsValid)
-            {
-                platform.Id = Guid.NewGuid();
-                _context.Add(platform);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(platform);
-        }
-
-        // GET: Platforms/Edit/5
-        public async Task<IActionResult> Edit(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var platform = await _context.Platform.FindAsync(id);
-            if (platform == null)
-            {
-                return NotFound();
-            }
-            return View(platform);
-        }
-
-        // POST: Platforms/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name")] Platform platform)
-        {
-            if (id != platform.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(platform);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PlatformExists(platform.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(platform);
-        }
-
-        // GET: Platforms/Delete/5
-        public async Task<IActionResult> Delete(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var platform = await _context.Platform
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (platform == null)
-            {
-                return NotFound();
-            }
-
-            return View(platform);
-        }
-
-        // POST: Platforms/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
-        {
-            var platform = await _context.Platform.FindAsync(id);
-            if (platform != null)
-            {
-                _context.Platform.Remove(platform);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool PlatformExists(Guid id)
-        {
-            return _context.Platform.Any(e => e.Id == id);
-        }
-    }
-}
-*/
