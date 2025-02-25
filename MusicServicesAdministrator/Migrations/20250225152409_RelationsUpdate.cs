b73e985e4f437ff9a53610a2e8f8a5bcabb4c005
IsmaelReta <ismaelreta.programacion@gmail.com>
2025-02-25 12:35:46 -0300

    Migrations

---
+++ MusicServicesAdministrator/Migrations/20250225152409_RelationsUpdate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicServicesAdministrator.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelationsUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlaylistSong",
+                columns: table => new
+                {
+                    PlaylistsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SongsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlaylistSong", x => new { x.PlaylistsId, x.SongsId });
+                    table.ForeignKey(
+                        name: "FK_PlaylistSong_Playlists_PlaylistsId",
+                        column: x => x.PlaylistsId,
+                        principalTable: "Playlists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlaylistSong_Songs_SongsId",
+                        column: x => x.SongsId,
+                        principalTable: "Songs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlaylistSong_SongsId",
+                table: "PlaylistSong",
+                column: "SongsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlaylistSong");
+        }
+    }
+}
---
+++ MusicServicesAdministrator/Migrations/20250225152533_ColumnRename.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicServicesAdministrator.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlayListId",
+                table: "Playlists",
+                newName: "PlayListPlatformId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlayListPlatformId",
+                table: "Playlists",
+                newName: "PlayListId");
+        }
+    }
+}
